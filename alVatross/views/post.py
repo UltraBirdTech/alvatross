e3130df6e0584849eec647807eec60dbe0b6b51a
UltraBirdTech <ultrabirdrepti@gmail.com>
2023-09-17 08:43:36 +0900

    add post

--- alVatross/views/post.py
+++ alVatross/views/post.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+from ..models.post import Post
+
 @login_required
 def index(request):
     params = {}
+    post_list = Post.objects.all()
+    params['post_list'] = post_list
     return render(request, 'alVatross/post.html', params)
+
+def insert(request):
+    pass
+
+def update(request):
+    pass
+
+def delete(request):
+    pass
