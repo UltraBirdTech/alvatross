bd4b2a8e046068cbe8df4fdeb8cefd98c0a29bd5
UltraBirdTech <ultrabirdrepti@gmail.com>
2023-09-17 08:43:18 +0900

    add migrate files

--- alVatross/models/post.py
+++ alVatross/models/post.py
@@ -5,11 +5,11 @@ from django.utils import timezone
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField(max_length=5000)
-    status = models.CharField(max_lentgh=10) # active/delete 
+    status = models.CharField(max_length=10) # active/delete 
     user = models.ForeignKey(User, on_delete=models.CASCADE) # 外部キー
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    def __str__(self)
+    def __str__(self):
         return self.title
     
