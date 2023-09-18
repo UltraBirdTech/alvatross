19cf17e3b74f6c5e2e6b01473b3ee41c27ce6fce
UltraBirdTech <ultrabirdrepti@gmail.com>
2023-09-18 17:11:12 +0900

    add post insert rogic

--- alVatross/views/post.py
+++ alVatross/views/post.py
@@ -2,16 +2,35 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 from ..models.post import Post
+from ..form.post import AddPostForm
 
 @login_required
 def index(request):
     params = {}
     post_list = Post.objects.all()
+    print(post_list)
     params['post_list'] = post_list
     return render(request, 'alVatross/post.html', params)
 
 def insert(request):
-    pass
+    params = {
+        'add_post_form': AddPostForm(),
+        'user': request.user
+    }
+    if request.method == 'POST':
+        params["add_post_form"] = AddPostForm(data=request.POST)
+        if params["add_post_form"].is_valid():
+            # TODO: Add Validation.
+            post = Post(
+                title   = request.POST.get("title"),
+                content = request.POST.get("content"),
+                status  = "active",
+                user_id = request.POST.get("user_id"),
+            )
+            post.save()
+            return render(request, 'alVatross/post.html', params)
+
+    return render(request, 'alVatross/insert_post.html', params)
 
 def update(request):
     pass
