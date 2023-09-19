ad071026bdc251cb285e255f2f6d704bb6d8f5f5
UltraBirdTech <ultrabirdrepti@gmail.com>
2023-09-20 08:06:44 +0900

    edit

--- alVatross/views/post.py
+++ alVatross/views/post.py
@@ -8,7 +8,6 @@ from ..form.post import AddPostForm
 def index(request):
     params = {}
     post_list = Post.objects.all()
-    print(post_list)
     params['post_list'] = post_list
     return render(request, 'alVatross/post.html', params)
 
@@ -19,14 +18,13 @@ def insert(request):
     }
     if request.method == 'POST':
         params["add_post_form"] = AddPostForm(data=request.POST)
-        if params["add_post_form"].is_valid():
-            # TODO: Add Validation.
-            post = Post(
-                title   = request.POST.get("title"),
-                content = request.POST.get("content"),
-                status  = "active",
-                user_id = request.POST.get("user_id"),
-            )
+        post = Post(
+            title   = request.POST.get("title"),
+            content = request.POST.get("content"),
+            status  = "active",
+            user_id = request.POST.get("user_id"),
+        )
+        if post.clean():
             post.save()
             return render(request, 'alVatross/post.html', params)
 
