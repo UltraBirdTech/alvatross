a7d217fa979211086490fa8e8731559aa43252b2
UltraBirdTech <ultrabirdrepti@gmail.com>
2023-10-24 07:41:36 +0900

    add eidt form

--- alVatross/form/post.py
+++ alVatross/form/post.py
@@ -5,3 +5,8 @@ class AddPostForm(forms.ModelForm):
     class Meta():
         model = Post
         fields = ('title', 'content')
+
+class EditPostForm(forms.ModelForm):
+    class Meta():
+        model = Post
+        fields = ('title', 'content')
