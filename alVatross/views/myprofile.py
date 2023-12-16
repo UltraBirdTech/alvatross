fee85c2c7412a2133855511da67210429370c503
UltraBirdTech <ultrabirdrepti@gmail.com>
2023-12-16 15:42:45 +0900

    add

--- alVatross/views/myprofile.py
+++ alVatross/views/myprofile.py
@@ -4,26 +4,56 @@ from django.contrib.auth.decorators import login_required
 
 from django.contrib.auth.models import User
 
-from django.template import Engine
+from django.template import Context, Engine, loader
 from django.template import RequestContext
 from django.http import HttpResponse
 
 @login_required
-def index(request, id):
-    user = User.objects.get(id=id)
+def index(request):
     params = {
-        'login_user': request.user
+        'login_user': request.user,
     }
 
-    template = '<!DOCTYPE html><html><body>\
+    #template_engine = Engine()
+   # layout_template = loader.get_template('layout.html')
+    index_template = loader.get_template('myprofile.html')
+    index_html = index_template.render(params)
+    print(index_html)
+    index_html = index_html.replace('#LAST_NAME#', request.user.last_name)
+    index_html = index_html.replace('#FIRST_NAME#', request.user.first_name)
+    print(index_html)
+   # result_html = layout_template.render({'content': index_html})
+    #return HttpResponse(result_html)
+    
+    
+    return HttpResponse(index_html)
+
+
+    template_html = '<!DOCTYPE html><html><body>\
+    <div class="test">\
+    {% block layout %}{layout_template.render(params)}{% endblock %}\
+    {layout_template}\
+    </div>\
+    {% block title %} alVatross {% endblock %}\
+    {% block content %}\
     <form action="" method="post">\
       First name:<br>\
       <input type="text" name="name" value="">\
       <input type="submit" value="Submit">\
-    </form><h2>Hello '+user.username+'</h2></body></html>'
+    </form><h2>Hello '+request.user.username+'</h2>\
+    {% endblock %}\
+    </body></html>'
+    base_template = template_engine.from_string(template_code=template_html)
 
-    engine = Engine()
-    template = engine.from_string(template_code=template)
-    context = RequestContext(request)
-    #context.push({"name": user.first_name})
-    return HttpResponse(template.render(context))
+
+    base_template = loader.get_template('index.html')
+    params = {
+        'login_user': request.user,
+        'layout': layout_template
+    }
+
+    context = Context(params)
+#    result_html = base_template.render({'layout': layout_template.render(context)})
+    result_html = base_template.render(context)
+   
+    return HttpResponse(result_html)
