53f6c67e7f6c831edd2e0c94d2bc13deefb02a7f
UltraBirdTech <ultrabirdrepti@gmail.com>
2024-02-12 16:40:22 +0900

    delete codes

--- alVatross/views/myprofile.py
+++ alVatross/views/myprofile.py
@@ -2,17 +2,10 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
 
-from django.contrib.auth.models import User
-
-from django.template import Context, Engine, loader
-from django.template import RequestContext
-from django.http import HttpResponse
-
 @login_required
 def index(request):
     params = {
         'login_user': request.user,
     }
-    print(request.user)
 
     return render(request, 'alvatross/myprofile.html', params)
